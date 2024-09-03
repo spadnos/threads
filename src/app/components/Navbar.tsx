89c6b98ba225aa07bbac78d6444c66b2cb1df3cf
Steve Padnos <spadnos@tanzanitesi.com>
2024-09-03 13:37:12 -0700

    Add avatar to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import Image from "next/image";
 
 const ACTIVE_ROUTE = "py-1 px-2 text-gray-300 bg-gray-700";
 const INACTIVE_ROUTE =
@@ -8,9 +9,26 @@ const INACTIVE_ROUTE =
 export default async function Navbar() {
   const session = await getServerSession();
 
+  const user = session?.user;
+  console.log(user);
+
+  const userImage = user?.image ? (
+    <div className="flex gap-4 items-center">
+      <Link href="/api/auth/signout">Sign Out</Link>
+      <Image
+        className="border-1 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto"
+        src={user?.image}
+        width={48}
+        height={48}
+        alt={user?.name ?? "Profile Pic"}
+        priority={true}
+      />
+    </div>
+  ) : null;
+
   return (
     <nav className="bg-blue-800 p-4">
-      <ul className="flex justify-evenly text-2xl font-bold">
+      <ul className="flex justify-evenly items-center text-2xl font-bold">
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -33,11 +51,8 @@ export default async function Navbar() {
             </li>
           </div>
         )}
-        {session && (
-          <li>
-            <Link href="/api/auth/signout">Sign Out</Link>
-          </li>
-        )}
+
+        {userImage}
       </ul>
     </nav>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
-import { options } from "./api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
-import UserCard from "./components/UserCard"
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import UserCard from "./components/UserCard";
 
 export default async function Home() {
-  const session = await getServerSession(options)
+  const session = await getServerSession(options);
 
+  // What the home page displays when the user is authenticated?
+  if (session) {
+    return <UserCard user={session?.user} pagetype={"Home"} />;
+  }
+
+  // What the home page displays when the user is not authenticated
   return (
-    <>
-      {session ? (
-        <UserCard user={session?.user} pagetype={"Home"} />
-      ) : (
-        <h1 className="text-5xl">You Shall Not Pass!</h1>
-      )}
-    </>
-  )
+    <div className="flex flex-col items-center container">
+      <h1 className="text-5xl">Home Page</h1>
+      <p className="mt-8 text-left">
+        This is what you will see when you are not authenticated.
+      </p>
+    </div>
+  );
 }
