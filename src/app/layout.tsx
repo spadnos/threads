31908d2a969920c1bf944f6ce6e1b198976d4c46
Steve Padnos <spadnos@tanzanitesi.com>
2024-09-05 14:31:19 -0700

    Cleanup layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
-import AuthProvider from "./context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "next-themes";
 import Providers from "./providers";
 
 export const metadata = {
-  title: "SeetulaComments",
+  title: "SeetulaThreads",
   description: "Playground for testing comment threads.",
 };
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -18,13 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen font-sans antialiased">
+      <body className={inter.className}>
         <Providers>
           <div className="container mx-auto flex flex-col h-screen">
             <Navbar />
-            <main className="container mb-auto mx-auto max-w-7xl pt-8 flex-grow">
-              {children}
-            </main>
+            <main className="container mb-auto mx-auto pt-8">{children}</main>
             <footer className="h-10 flex justify-center py-3">
               Copyright &copy; 2024 Seetula
             </footer>
